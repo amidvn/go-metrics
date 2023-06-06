c89daf95a1ebcfec104a1a0ee7e7da734c506dd1
Dmitriy Vorontsov <dmitriy1105@gmail.com>
2023-06-06 14:47:40 +0300

    Change params for rewriting

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -94,7 +94,7 @@ type Producer struct {
 }
 
 func NewProducer(fileName string) (*Producer, error) {
-	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
 	}

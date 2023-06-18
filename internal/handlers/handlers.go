9916c103e590bfa547f7a5f673fea294a0f41940
Dmitriy <dvnoro@gmail.com>
2023-06-18 16:28:21 +0300

    debugging

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -136,14 +136,12 @@ func UpdatesJSON(s *storage.MemStorage) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		metrics := make([]models.Metrics, 0)
 		err := json.NewDecoder(ctx.Request().Body).Decode(&metrics)
-		fmt.Println("error", err)
 		if err != nil && err != io.EOF {
 			return ctx.String(http.StatusBadRequest, fmt.Sprintf("Error in JSON decode: %s", err))
 		}
 
 		s.StoreBatch(metrics)
 
-		ctx.Response().Header().Set("Content-Type", "application/json")
-		return ctx.JSON(http.StatusOK, metrics)
+		return ctx.NoContent(http.StatusOK)
 	}
 }
